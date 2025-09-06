a3f5f939d446bb4083917a1e61840086e7d777e7
kvoskresenskiy <k_voskresenskiy@bk.ru>
2025-09-07 02:58:40 +0300

    Good_sort done

---
+++ algorithms/home task 06.09.25/Good_sort(ex1).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+int main() {
+int n;
+std :: cin >> n;
+if (n <= 2) {
+   std :: cout << "При n<=2 алгоритм работает верно для любого массива." << std :: endl;
+} else {
+  std :: vector<int> a;
+  for (int i = 2; i <= n; i++) {
+    a.push_back(i);
+}
+  a.push_back(1);
+  std :: cout << "Контрпример: " << std :: endl;
+  for (int i = 0; i < n; i++) {
+     std :: cout << a[i] << " "<< std :: endl;
+}
+}
+return 0;
+}
