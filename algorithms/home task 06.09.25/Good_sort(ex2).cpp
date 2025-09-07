5f8a50d2d0e5f1a598f1fd9feb69944b1d33c360
kvoskresenskiy <k_voskresenskiy@bk.ru>
2025-09-07 23:03:11 +0300

    Good_sort(ex2) new

--- algorithms/home task 06.09.25/Good_sort(ex2).cpp
+++ algorithms/home task 06.09.25/Good_sort(ex2).cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <vector>
 int main() {
     int n;
     std :: cin >> n;
-    int a[n];
     if (n <= 2) {
    std :: cout << "При n<=2 алгоритм работает верно для любого массива." << std :: endl;
 } else {
-    for (int i = 0; i < n; i++) {
-        std :: cin >> a[i];
+   std :: vector<int> a;
+  for (int i = 1; i <= n; i++) {
+    a.push_back(i);
     }
       std :: cout << "Контрпример: " << std :: endl;
     for (int i = n - 1; i >= 0; i--) {
