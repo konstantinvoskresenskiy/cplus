ea30e2512a48f84329d26ce0c82cadfc8bc21e62
kvoskresenskiy <k_voskresenskiy@bk.ru>
2025-10-17 14:36:44 +0300

    queries_on_segment refactoring done

--- modern_programming/queries_on_segment_refactoring/code_refactoring.cpp
+++ modern_programming/queries_on_segment_refactoring/code_refactoring.cpp
@@ -1,92 +1,101 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <math.h>
 #include <random>
-using namespace std;
+
 typedef long long ll;
-void vout(vector <int> &v){
-    for (auto i : v){
-        cout << i << " ";
-    }
-    cout << endl;
-}
 
-void vin(vector <int> &v,int n){
-    for(int i = 0; i < n;i++){
-        int x;
-        cin >> x;
-        v.push_back(x);
+void arr_out(int* v, int size);
+void arr_in(int* &v, int n);
+void random_arr(int* &out, int sz, int max_el_size);
+void prefix_sum_arr(int* v, int size);
+int arr_max(int* v, int size);
+int* count_sort(int* v, int size);
+int interval(int* v, int l, int r);
+
+int main(){
+    int* arr = nullptr;
+    int size, max_el;
+    std :: cout << "Введите размерность и максимальный элемент случайного вектора: ";
+    std :: cin >> size >> max_el;
+    random_arr(arr, size, max_el);
+    std :: cout << "Сгенерированный вектор: ";
+    arr_out(arr, size);
+    
+    int* sorted = count_sort(arr, size);
+    int sorted_size = arr_max(arr, size) + 1;
+    std :: cout << "Отсортированный вектор(вектор частот): ";
+    arr_out(sorted, sorted_size);
+    
+    std :: cout << "Вектор префиксных сумм: ";
+    prefix_sum_arr(sorted, sorted_size);
+    arr_out(sorted, sorted_size);
+    
+    int queries;
+    std :: cout << "Введите количество будущих запросов: ";
+    std :: cin >> queries;
+    std :: cout << std :: endl;
+    
+    for(int i = 0; i < queries; i++){
+        std ::cout << "Введите левую и правую границы через пробел: ";
+        int left, right;
+        std :: cin >> left >> right;
+        std :: cout << "Количество чисел в промежутке [l;r] равно: " << interval(sorted, left, right) << std :: endl;
+    }
+
+    delete[] arr;
+    delete[] sorted;
+    
+    return 0;
+}
+void arr_out(int* arr, int size){
+    for (int i = 0; i < size; i++){
+        std :: cout << arr[i] << " ";
+    }
+    std :: cout << std :: endl;
+}
+void arr_in(int* &arr, int n){
+    arr = new int[n];
+    for(int i = 0; i < n; i++){
+        std::cin >> arr[i];
     }
 }
-
-void rnd_v(vector <int> &out,int sz,int max_el_size){
-    mt19937 rnd(time(NULL));
-    for(int i = 0;i<sz;i++){
-        out.push_back(rnd()%max_el_size);
+void random_arr(int* &out, int sz, int max_el_size){
+    out = new int[sz];
+    std :: mt19937 rnd(time(NULL));
+    for(int i = 0; i < sz; i++){
+        out[i] = rnd() % max_el_size;
     }
 }
-
-int vmax(vector <int> &v){
+int arr_max(int* arr, int size){
     int n = -1;
-    for (auto i : v){
-        n = max(n,i);
+    for (int i = 0; i < size; i++){
+        n = std :: max(n, arr[i]);
     }
     return n;
 }
-
-vector <int> count_sort(vector <int> &v){
-    int n = v.size();
-    vector <int> out(vmax(v));
-    for(auto i: v){
-        out[i] += 1;
+int* count_sort(int* arr, int size){
+    int max_val = arr_max(arr, size);
+    int* out = new int[max_val + 1]();
+    for(int i = 0; i < size; i++){
+        out[arr[i]] += 1;
     }
     return out;
 }
-
-void prefix_v(vector <int> &v){
-    int n = v.size();
-    for(int i = 0;i<n;i++){
-        if(i!=0){
-            v[i] += v[i-1];
+void prefix_sum_arr(int* arr, int size){
+    for(int i = 0; i < size; i++){
+        if(i != 0){
+            arr[i] += arr[i-1];
         }
     }
 }
-
-int interval(vector <int> &v,int &l,int &r){
+int interval(int* arr, int l, int r){
     int left;
     if(l == 0){
         left = 0;
-    }else{
-        left = v[l-1];
+    } else {
+        left = arr[l-1];
     }
-    int right = v[r];
+    int right = arr[r];
     return right - left;
-}
-
-int main(){
-    vector <int> v;
-    int sz,max_el;
-    cout << "Введите размерность и максимальный элемент случайного вектора: ";
-    cin >> sz >> max_el;
-    rnd_v(v,sz,max_el);
-    cout << "Сгенерированный вектор: ";
-    vout(v);
-    vector <int> sorted = count_sort(v);
-    cout << "Отсортированный  вектор(вектор частот): ";
-    vout(sorted);
-    cout << "Вектор префиксных сумм: ";
-    prefix_v(sorted);
-    vout(sorted);
-    int q;
-    cout << "Введите количество будущих запросов: ";
-    cin >> q;
-    cout << endl;
-    for(int i = 0;i<q;i++){
-        cout << "Введите левую и правую границы через пробел: ";
-        int l,r;
-        cin >> l >> r;
-        cout << "Количество чисел в промежутке [l;r] равно: " << interval(sorted,l,r) << endl;
-    }
-    return 0;
 }
